e373b111c14acf34668a28bca42d4d96a28edc57
JoSangYeon <whtkddus98@kookmin.ac.kr>
2022-06-01 19:05:20 +0900

    update main.py

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-import torch
-from MyModule.ChatBot import My_ChatBot
+from ChatBot import My_ChatBot
+from AE.AE_Bank import *
 
 def main():
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
-    model = My_ChatBot(base_path='data/', device=device)
+    model = My_ChatBot(device=device, embed_dim=256)
     model.to(device)
 
     model.chat(True, True)
